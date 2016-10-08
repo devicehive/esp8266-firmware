9cd0c36f99ea79e086bce6518cfde9f72a6e9dd0
Nikolay Khabarov <2xl@mail.ru>
2016-10-08 22:23:40 +0300

    rest prevent interaption commands

--- firmware-src/sources/rest.c
+++ firmware-src/sources/rest.c
@@ -11,6 +11,7 @@
 #include <osapi.h>
 #include "rest.h"
 #include "dhrequest.h"
+//#include "dhcommands.h"
 
 static const char desription[] = "<html><body>This is firmware RESTfull API endpoint. "\
 	"Please follow the firmware manual to use it.<br><a href='http://devicehive.com/' "\
@@ -20,6 +21,7 @@ static const char desription[] = "<html><body>This is firmware RESTfull API endp
 
 HTTP_RESPONSE_STATUS ICACHE_FLASH_ATTR rest_handle(const char *path, const char *key,
 		HTTP_CONTENT *content_in, HTTP_CONTENT *content_out) {
+	static const char cint[] = "/int";
 	if(path[0] == 0) {
 		content_out->data = desription;
 		content_out->len = sizeof(desription) - 1;
@@ -29,11 +31,20 @@ HTTP_RESPONSE_STATUS ICACHE_FLASH_ATTR rest_handle(const char *path, const char 
 		if(key == 0) {
 			return HRCS_UNAUTHORIZED;
 		}
-		if(os_strcmp(key,dhrequest_current_accesskey())) {
+		if(os_strcmp(key, dhrequest_current_accesskey())) {
 			return HRCS_UNAUTHORIZED;
 		}
 	}
 
+	// prevent all commands with interruption
+	int pathlen = os_strlen(path);
+	if(pathlen >= sizeof(cint) - 1) {
+		if(os_strncmp(&path[pathlen - sizeof(cint) + 1], cint, sizeof(cint) - 1) == 0) {
+			return HRCS_NOT_FOUND;
+		}
+	}
+
 	// TODO
-	return HRCS_NOT_IMPLEMENTED;
+	//dhcommands_do(0, path, content_in->data, content_in->len);
+	return HRCS_NOT_FOUND;
 }
