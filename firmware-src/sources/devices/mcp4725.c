24cd671260094f97293acf5b2b63d839b8b5a846
Nikolay Khabarov <2xl@mail.ru>
2016-11-25 19:41:41 +0300

    mcp4725 module

---
+++ firmware-src/sources/devices/mcp4725.c
@@ -0,0 +1,53 @@
+/*
+ * mcp4725.c
+ *
+ * Copyright 2016 DeviceHive
+ *
+ * Author: Nikolay Khabarov
+ *
+ */
+
+#include <osapi.h>
+#include <c_types.h>
+#include "mcp4725.h"
+#include "dhi2c.h"
+#include "dhdebug.h"
+#include "dhutils.h"
+
+static int mAddress = MCP4725_DEFAULT_ADDRESS;
+static float mVoltage = 3.3f;
+
+DHI2C_STATUS ICACHE_FLASH_ATTR mcp4725_write(int sda, int scl, float value) {
+	char buf[2];
+	DHI2C_STATUS status;
+	int channel;
+	if(value < 0.0f || value > mVoltage)
+		return DHI2C_WRONG_PARAMETERS;
+	if(sda != MCP4725_NO_PIN && scl != MCP4725_NO_PIN) {
+		if((status = dhi2c_init(sda, scl)) != DHI2C_OK) {
+			dhdebug("mcp4725: failed to set up pins");
+			return status;
+		}
+	}
+
+	uint16_t v = (uint16_t)(value / mVoltage * 4096.0f);
+
+	buf[0] = 0x40 | ((v >> 8) & 0x0F); // Config(write DAC register), MSB 4 bits
+	buf[1] = (v & 0xFF); // LSB bits
+	if((status = dhi2c_write(mAddress, buf, 2, 1)) != DHI2C_OK) {
+		dhdebug("mcp4725: failed to write");
+		return status;
+	}
+	return DHI2C_OK;
+}
+
+void ICACHE_FLASH_ATTR mcp4725_set_address(int address) {
+	mAddress = address;
+}
+
+DHI2C_STATUS ICACHE_FLASH_ATTR mcp4725_set_vref(float voltage) {
+	if(mVoltage <= 0.0f)
+		return DHI2C_WRONG_PARAMETERS;
+	mVoltage = voltage;
+	return DHI2C_OK;
+}
---
+++ firmware-src/sources/devices/mcp4725.h
@@ -0,0 +1,42 @@
+/**
+ *	\file		mcp4725.h
+ *	\brief		Simple communication with MCP4725 DAC
+ *	\author		Nikolay Khabarov
+ *	\date		2016
+ *	\copyright	DeviceHive MIT
+ */
+
+#ifndef SOURCES_DEVICES_MCP4725_H_
+#define SOURCES_DEVICES_MCP4725_H_
+
+#include "dhi2c.h"
+
+/** Default sensor i2c address*/
+#define MCP4725_DEFAULT_ADDRESS 0xC0
+/** Do not initialize pin */
+#define MCP4725_NO_PIN -1
+
+/**
+ *	\brief					Set DAC voltages.
+ *	\param[in]	sda			Pin for I2C's SDA.
+ *	\param[in]	scl			Pin for I2C's SCL.
+ *	\param[out]	value		Voltage in Volts.
+ *	\return 				Status value, one of DHI2C_STATUS enum.
+ */
+DHI2C_STATUS mcp4725_write(int sda, int scl, float value);
+
+/**
+ *	\brief					Set sensor address which should be used while reading.
+ *	\param[in]	address		Pin for I2C's SDA.
+ */
+void mcp4725_set_address(int address);
+
+/**
+ *	\brief					Set reference voltage in volts.
+ *	\note					Default is 3.3V.
+ *	\param[in]	voltage		Voltage in Volts.
+ *	\return 				Status value, one of DHI2C_STATUS enum.
+ */
+DHI2C_STATUS mcp4725_set_vref(float voltage);
+
+#endif /* SOURCES_DEVICES_MCP4725_H_ */
