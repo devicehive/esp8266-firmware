81474345a7e2f33ecdc228783902c6d6b75e8b6e
Igor Malovitsa <igor.malovitsa@dataart.com>
2016-12-29 12:01:59 +0200

    cleaned up restlib a little, fixed js lint erorrs in editor and tryapi

--- firmware-src/pages/librest.js
+++ firmware-src/pages/librest.js
@@ -1,24 +1,27 @@
-function rest_xmlhttp(host, key, command, params, cb) {
+function request_xhr(verb, url, params, key, cb) {
 	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open('POST', "http://" + host + '/api/' + command, true);
-	xmlhttp.setRequestHeader("Authorization", "Bearer " + key);
+	xmlhttp.open(verb, url);
+	xmlhttp.setRequestHeader('Authorization', 'Bearer ' + key);
 	xmlhttp.onreadystatechange = function () {
 		if (xmlhttp.readyState == 4)
 			cb(xmlhttp);
 	};
-	if(params == null || params == undefined)
+	if(params == null || verb.match(/^get$/i))
 		xmlhttp.send();
 	else if(params.length == 0)
 		xmlhttp.send();
-	else
-		xmlhttp.send(JSON.stringify(params));
+	else {
+		if (typeof params == 'object')
+			params = JSON.stringify(params);
+		xmlhttp.send(params);
+	}
 }
 
-function rest(host, key, command, params, cb) {
-	rest_xmlhttp(host, key, command, params, function(xmlhttp) {
-		var res = '';
+function rest(command, params, key, cb) {
+	rest_xmlhttp('POST', '/api/' + command, params, key, function(xmlhttp) {
+		var res = xmlhttp.responseText;
 		try {
-			res = JSON.parse(xmlhttp.responseText);
+			res = JSON.parse(res);
 		} catch (e) { }
 		if (xmlhttp.status < 200 || xmlhttp.status > 299)
 			cb(true, res);
@@ -28,7 +31,7 @@ function rest(host, key, command, params, cb) {
 }
 
 function rest_common(command, params, cb) {
-	key = document.getElementById('accesskey').value;
+	var key = document.getElementById('accesskey').value;
 	localStorage['accesskey'] = key;
-	rest(window.location.hostname, key, command, params, cb);
+	rest(command, params, key, cb);
 }
