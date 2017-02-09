3d3df95df50f99a1dfdddba890924900a862adac
Nikolay Khabarov <2xl@mail.ru>
2017-02-09 20:01:07 +0300

    simplify calculation

--- firmware-src/sources/devices/max6675.c
+++ firmware-src/sources/devices/max6675.c
@@ -12,6 +12,7 @@
 #include "max6675.h"
 #include "dhspi.h"
 #include "dhgpio.h"
+#include "dhutils.h"
 
 static int mCSPin = 15;
 
@@ -27,12 +28,12 @@ char * ICACHE_FLASH_ATTR max6675_read(int pin, float *temperature) {
 			return "Wrong CS pin";
 		mCSPin = pin;
 	}
+	dhspi_set_mode(SPI_CPOL0CPHA0);
 
 	//start converting
 	dhgpio_write((1 << mCSPin), 0);
 	os_delay_us(250000);
 
-	dhspi_set_mode(SPI_CPOL0CPHA0);
 	dhspi_read((char *)&buf, sizeof(buf));
 
 	if ((buf[0] & 0x80) || (buf[1] & 0x02))
@@ -40,9 +41,9 @@ char * ICACHE_FLASH_ATTR max6675_read(int pin, float *temperature) {
 	if (buf[1] & 0x04)
 		return "Thermocouple is not connected";
 
-	int v = (buf[1] >> 3) & 0x1F;
-	v += (((int)buf[0]) & 0x7F) * 0x20;
-	*temperature = v / 4.0f;
+	buf[1] &= 0xF8;
+	int v = signedInt16be(buf, 0);
+	*temperature = v / 32.0f;
 
 	return NULL;
 }
