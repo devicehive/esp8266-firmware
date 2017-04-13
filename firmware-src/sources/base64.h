738d3bd3b4a59ed61821d055d45c57f675fd58e3
Sergey Polichnoy <pilatuz@gmail.com>
2017-04-14 00:06:38 +0300

    review the base64.h module

--- firmware-src/sources/base64.h
+++ firmware-src/sources/base64.h
@@ -1,55 +1,67 @@
 /**
- *	\file		base64.h
- *	\brief		Base64 encode/decode implementation.
- *	\author		Nikolay Khabarov
- *	\date		2015
- *	\copyright	Public Domain
+ * @file
+ * @brief Base64 encode/decode.
+ * @copyright 2017 [DeviceHive](http://devicehive.com)
+ * @author Nikolay Khabarov
  */
-
 #ifndef _DATAENCODEBASE64_H_
 #define _DATAENCODEBASE64_H_
 
-#include "user_config.h"
+#include <c_types.h>
 
-#ifdef DATAENCODEBASE64
-
-/**
- *	\brief				Encode binary data to text with Base64.
- *	\details			Function check if output buffer enough for text and start writing to it only this check. Return zero immediately otherwise.
- *	\param[in]	data	Pointer to binary data.
- *	\param[in]	datalen	Data size in bytes.
- *	\param[out]	out		Pointer to output buffer.
- *	\param[in]	outlen	Output buffer size in bytes.
- *	\return 			Number of chars that was copied to output buffer.
- */
-int base64_encode(const char *data, unsigned int datalen, char *out, unsigned int outlen);
 
 /**
- *	\brief				Report how many bytes will take encoded data.
- *	\param[in]	datalen	Binary data size in bytes.
- *	\return 			Base64 encoded size in bytes.
+ * @brief Encode binary data to text using `Base64` encoding.
+ *
+ * Function checks if length of output buffer is enough to store `Base64`
+ * output text. If output buffer length is too small nothing is written.
+ *
+ * @param[in] data Pointer to binary data.
+ * @param[in] data_len Binary data length in bytes.
+ * @param[out] text Pointer to output buffer.
+ * @param[in] text_len Output buffer length in bytes.
+ * @return Number of bytes written to output buffer.
  */
-unsigned int base64_encode_length(unsigned int datalen);
+int base64_encode(const void *data,
+                  size_t data_len,
+                  char *text,
+                  size_t text_len);
+
 
 /**
- *	\brief				Decode text with Base64 to binary data.
- *	\details			Function check if output buffer enough for data and start writing to it only this check. Return zero immediately otherwise.
- *	\param[in]	data	Pointer to Base64 encoded text.
- *	\param[in]	datalen	Base64 encoded text size in bytes.
- *	\param[out]	out		Pointer to output buffer.
- *	\param[in]	outlen	Output buffer size in bytes.
- *	\return 			Number of bytes that was copied to output buffer.
+ * @brief Estimate how many bytes will take `Base64` encoded data.
+ * @param[in] data_len Binary data length in bytes.
+ * @return `Base64` encoded text length in bytes.
  */
-int base64_decode(const char *data, unsigned int datalen, char *out, unsigned int outlen);
+size_t base64_encode_length(size_t data_len);
+
 
 /**
- *	\brief				Report how many bytes will take decoded data.
- *	\param[in]	data	Pointer to Base64 encoded text.
- *	\param[in]	datalen	Base64 encoded text size in bytes.
- *	\return 			Decoded binary data size in bytes.
+ * @brief Decode text to binary data using `Base64` encoding.
+ *
+ * Function checks if length of output buffer is enough to store `Base64`
+ * decoded data. If output buffer length is too small nothing is written.
+ * @param[in] text Pointer to `Base64` encoded text.
+ * @param[in] text_len Encoded text length in bytes. Should be multiple of 4.
+ * @param[out] data Pointer to output binary data buffer.
+ * @param[in] data_len Output buffer length in bytes.
+ * @return Number of bytes written to output buffer.
  */
-unsigned int base64_decode_length(const char *data, unsigned int datalen);
+int base64_decode(const char *text,
+                  size_t text_len,
+                  void *data,
+                  size_t data_len);
 
-#endif //DATAENCODEBASE64
+
+/**
+ * @brief Estimate how many bytes will take base64 decoded data.
+ *
+ * Encoded data is used to check padding characters at the end.
+ *
+ * @param[in] text Pointer to base64 encoded text.
+ * @param[in] text_len Encoded text length in bytes.
+ * @return Decoded binary data length in bytes.
+ */
+size_t base64_decode_length(const char *text, size_t text_len);
 
 #endif /* _DATAENCODEBASE64_H_ */
